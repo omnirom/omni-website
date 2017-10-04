ce9e5b76877e9bcba23f51c497b2ab0bebad1099
jerdog <jeremy.meiss@gmail.com>
2017-10-03 21:49:52 -0500

    change wording to remove old content around contributors and old links

--- src/containers/source/Source.jsx
+++ src/containers/source/Source.jsx
@@ -21,14 +21,12 @@ export class Source extends Component {
         </div>
 
         <div className="source__section">
-          <h2 className="source__section-title">Contributors</h2>
+          <h2 className="source__section-title">Get Involved</h2>
           <div className="source__section-image">
             <img src={android} />
           </div>
           <div className="source__section-text">
-            Below is a list of contributors to Omni that is automatically generated from merged Gerrit change requests. If you would like to contribute, make sure you check out our <a href="http://docs.omnirom.org/Getting_Involved" target="_blank">Getting Involved</a>, <a href="http://docs.omnirom.org/Gerrit_Rules" target="_blank">Gerrit Rules</a>, and <a href="http://docs.omnirom.org/Contributing_code" target="_blank">Contributing Code</a> pages. We will update this list on a regular basis, so look to see this list grow!
-
-            <p>**automatically generated list**</p>
+            Omni exists because of the contributors to the project. If you would like to participate, make sure you check out our <a href="https://github.com/omnirom/Docs/blob/master/Contributing_code.md" target="_blank">Contributing Code</a> and <a href="https://github.com/omnirom/Docs/blob/master/Gerrit_rules.md" target="_blank">Gerrit Rules</a> pages. We will update this list on a regular basis, so look to see this list grow!
           </div>
         </div>
       </Page>
